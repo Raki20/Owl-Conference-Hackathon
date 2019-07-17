184f554bf66d5e2cd8b71e9676fe7a1ce4524362
Sofia <sofiaestanqueirorocha@gmail.com>
2019-07-17 22:58:49 +0100

    add title to banner and insert background image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Home = () => {
     return (
         <section id="home">
-            <h1>Owl Conference</h1>
+            <div className="banner">
+                <div className="banner-text">
+                    <h1>OWL CONFERENCE</h1>
+                    <h2>BRING IT OWL!</h2>
+                </div>
+                <h3>Powered by Mercedes-Benz.io</h3>
+            </div>
         </section>
     );
 };
