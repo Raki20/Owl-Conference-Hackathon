c7e4b41a3ce37888943a03bd31a26c00ec2b7f0f
Raquel Moreira <araquelmoreira@gmail.com>
2019-07-18 14:18:41 +0100

    adds footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+// import facebooklogo from '../../assets/icons/facebooklogo'
 
 const Footer = () => {
     return (
-        <div className="Footer">
+        <div className="footer">
+            <div className="footer-text">
+                <p> @ 2019 | Owl Conf | Stuttgart </p>
+            </div>
 
+            <div className="footer-icons">
+
+                <div>
+                    <a href="https://www.instagram.com/mercedesbenz.io/">
+                        <img src="../../assets/icons/instagram-logo-white.png" alt="instagram icon" />
+                    </a>
+                </div>
+
+                <div>
+                    <a href="https://www.facebook.com/mercedesbenziogmbh/">
+                        <img src="../../assets/icons/facebook.png" alt="facebook icon" />
+                    </a>
+                </div>
+
+                <div>
+                    <a href="https://twitter.com/mercedesbenzio">
+                        <img src="../../assets/icons/twitter.png" alt="twitter icon" />
+                    </a>
+                </div>
+
+                <div>
+                    <a href="https://github.com/mercedesbenzio">
+                        <img src="../../assets/icons/github.png" alt="github icon" />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
