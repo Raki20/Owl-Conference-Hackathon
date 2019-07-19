3a5b8c6b87f1ea86ea67b20b4b52bf341fcc3997
Sofia <sofiaestanqueirorocha@gmail.com>
2019-07-19 12:14:19 +0100

    hover btn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ import Jobs from "./Jobs";
 import SpeakerModal from "./SpeakerModal";
 import Loading from "./Loading";
 import Footer from "./Footer";
-// import apiStuff from '../../mercedesapi.json';
 
 
 class App extends Component {
@@ -27,12 +26,9 @@ class App extends Component {
     }
 
     componentDidMount () {
-        // setTimeout(() => {
-        //     this.setState({ ...apiStuff, isLoading: false });
-        // }, 1000);
         setTimeout(() => {
             this.getConferenceInfo();
-        }, 800);
+        }, 500);
         this.reportWindowSize();
         window.addEventListener('resize', this.reportWindowSize);
     }
